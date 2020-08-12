e13a261bd185b8b2ae298b69ac501279130326f2
Bruno Bach <42494225+brunobach@users.noreply.github.com>
2020-08-12 01:44:39 -0300

    Iniciando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import GlobalStyles from './styles/globalStyles'
+import Section from './components/Section'
+
+import data from './data'
+
 function App() {
   return (
     <div className="App">
-  
-          Learn React
+          <Section variant="blue" title={data[0].title} description={data[0].description} />
+          <Section variant="beige" title={data[1].title} description={data[1].description} />
+          <Section variant="blue" title={data[2].title} description={data[2].description} />
+          <Section variant="white" title={data[3].title} description={data[3].description} />
+          <Section variant="black" title={data[4].title} description={data[4].description} />
+          <GlobalStyles />
 
     </div>
   );
